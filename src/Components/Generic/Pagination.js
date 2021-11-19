d2e4a2961b3e31990c9048619636054d30bd151b
farklin <ivanhel@yandex.ru>
2021-11-19 17:31:02 +0300

    Разработка пагинации

---
+++ src/Components/Generic/Pagination.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Pagination } from "@material-ui/";
+
+function Pagination(current_page, last_page) {
+    return ( <
+        div >
+        <
+        Pagination count = { 10 }
+        />   <
+        /div >
+    )
+}
+
+export default Pagination
